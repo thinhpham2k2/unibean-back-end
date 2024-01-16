a4603c15784adfcd0e1a1b1f9956e14ea58f0a1d
@thinhphamquoc9999 <thinhphamquoc9999@gmail.com>
2024-01-16 19:31:45 +0700

    Update ActivityRepository

--- Unibean.Repository/Repositories/ActivityRepository.cs
+++ Unibean.Repository/Repositories/ActivityRepository.cs
@@ -1,7 +1,33 @@
-﻿using Unibean.Repository.Repositories.Interfaces;
+﻿using Unibean.Repository.Entities;
+using Unibean.Repository.Paging;
+using Unibean.Repository.Repositories.Interfaces;
 
 namespace Unibean.Repository.Repositories;
 
 public class ActivityRepository : IActivityRepository
 {
+    public Activity Add(Activity creation)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Delete(string id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public PagedResultModel<Activity> GetAll(List<string> storeIds, List<string> studentIds, List<string> voucherIds, string propertySort, bool isAsc, string search, int page, int limit)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Activity GetById(string id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Activity Update(Activity update)
+    {
+        throw new NotImplementedException();
+    }
 }
--- Unibean.Repository/Repositories/Interfaces/IActivityRepository.cs
+++ Unibean.Repository/Repositories/Interfaces/IActivityRepository.cs
@@ -1,5 +1,19 @@
-﻿namespace Unibean.Repository.Repositories.Interfaces;
+﻿using Unibean.Repository.Entities;
+using Unibean.Repository.Paging;
+
+namespace Unibean.Repository.Repositories.Interfaces;
 
 public interface IActivityRepository
 {
+    Activity Add(Activity creation);
+
+    void Delete(string id);
+
+    PagedResultModel<Activity> GetAll
+        (List<string> storeIds, List<string> studentIds, List<string> voucherIds, 
+        string propertySort, bool isAsc, string search, int page, int limit);
+
+    Activity GetById(string id);
+
+    Activity Update(Activity update);
 }
