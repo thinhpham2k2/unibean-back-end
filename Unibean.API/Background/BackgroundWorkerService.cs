5b896823a7bb048c564f6260ec27c8c65b678090
@thinhphamquoc9999 <thinhphamquoc9999@gmail.com>
2024-03-01 15:46:34 +0700

    Create Background Service

---
+++ Unibean.API/Background/BackgroundWorkerService.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Unibean.API.Background;
+
+public class BackgroundWorkerService : IHostedService
+{
+    readonly ILogger<BackgroundWorkerService> _logger;
+
+    public BackgroundWorkerService(ILogger<BackgroundWorkerService> logger)
+    {
+        _logger = logger;
+    }
+
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
