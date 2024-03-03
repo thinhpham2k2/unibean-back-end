a31654741e68fd145db45802e385589d32b22b32
@thinhphamquoc9999 <thinhphamquoc9999@gmail.com>
2024-03-03 10:14:01 +0700

    Update BackgroundWorkerService

--- Unibean.API/Background/BackgroundWorkerService.cs
+++ Unibean.API/Background/BackgroundWorkerService.cs
@@ -1,22 +1,31 @@
-﻿
+﻿using Unibean.Repository.Repositories.Interfaces;
+
 namespace Unibean.API.Background;
 
-public class BackgroundWorkerService : IHostedService
+public class BackgroundWorkerService : BackgroundService
 {
     readonly ILogger<BackgroundWorkerService> _logger;
 
-    public BackgroundWorkerService(ILogger<BackgroundWorkerService> logger)
+    private readonly ICampaignRepository campaignRepository;
+
+    private readonly ICampaignActivityRepository campaignActivityRepository;
+
+    public BackgroundWorkerService(
+        ILogger<BackgroundWorkerService> logger,
+        ICampaignRepository campaignRepository,
+        ICampaignActivityRepository campaignActivityRepository)
     {
         _logger = logger;
+        this.campaignRepository = campaignRepository;
+        this.campaignActivityRepository = campaignActivityRepository;
     }
 
-    public Task StartAsync(CancellationToken cancellationToken)
+    protected async override Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        throw new NotImplementedException();
-    }
-
-    public Task StopAsync(CancellationToken cancellationToken)
-    {
-        throw new NotImplementedException();
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            _logger.LogInformation("Worker running at: {time}", DateTime.UtcNow);
+            await Task.Delay(300000, stoppingToken);
+        }
     }
 }
