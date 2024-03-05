99a6a58a820589a3a59210b0b666f52cad23576f
@thinhphamquoc9999 <thinhphamquoc9999@gmail.com>
2024-03-05 22:24:09 +0700

    Update Line Chart API (1)

--- Unibean.Repository/Repositories/Interfaces/IBonusTransactionRepository.cs
+++ Unibean.Repository/Repositories/Interfaces/IBonusTransactionRepository.cs
@@ -6,4 +6,6 @@ public interface IBonusTransactionRepository
 {
     List<BonusTransaction> GetAll
         (List<string> walletIds, List<string> bonusIds, List<WalletType> walletTypeIds, string search);
+
+    decimal IncomeOfGreenBean(string storeId, DateOnly date);
 }
