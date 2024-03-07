fe9dfa4372a5f187610d1bf42fe3550831818071
@thinhphamquoc9999 <thinhphamquoc9999@gmail.com>
2024-03-07 13:45:57 +0700

    Done Test Case GetList, GetById, Create in AdminController

--- Unibean.Test/Controllers/AdminControllerTest.cs
+++ Unibean.Test/Controllers/AdminControllerTest.cs
@@ -1,9 +1,12 @@
 ﻿using FakeItEasy;
 using FluentAssertions;
+using Google.Apis.Util;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Unibean.API.Controllers;
 using Unibean.Repository.Paging;
 using Unibean.Service.Models.Admins;
+using Unibean.Service.Models.Exceptions;
 using Unibean.Service.Services.Interfaces;
 
 namespace Unibean.Test.Controllers;
@@ -45,5 +48,123 @@ public class AdminControllerTest
         // Assert
         result.Should().NotBeNull();
         result.Should().BeOfType(typeof(ActionResult<PagedResultModel<AdminModel>>));
+        Assert.Equal(StatusCodes.Status200OK, result.Result?.GetType().GetProperty("StatusCode")?.GetValue(result.Result));
+    }
+
+    [Fact]
+    public void AdminController_GetList_ReturnBadRequest()
+    {
+        // Arrange
+        var controller = new AdminController
+            (adminService, chartService, requestService, fireBaseService);
+
+        // Act
+        var result = controller.GetList(true, new()
+        {
+            Sort = "Ids,desc",
+            Search = "",
+            Page = 1,
+            Limit = 10,
+        });
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Should().BeOfType(typeof(ActionResult<PagedResultModel<AdminModel>>));
+        Assert.Equal(StatusCodes.Status400BadRequest, result.Result?.GetType().GetProperty("StatusCode")?.GetValue(result.Result));
+    }
+
+    [Fact]
+    public void AdminController_GetById_ReturnOK()
+    {
+        // Arrange
+        string id = "";
+        var controller = new AdminController
+            (adminService, chartService, requestService, fireBaseService);
+
+        // Act
+        var result = controller.GetById(id);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Should().BeOfType(typeof(ObjectResult));
+        Assert.Equal(StatusCodes.Status200OK,
+            result.GetType().GetProperty("StatusCode")?.GetValue(result));
+    }
+
+    [Fact]
+    public void AdminController_GetById_ReturnBadRequest()
+    {
+        // Arrange
+        string id = "";
+        A.CallTo(() => adminService.GetById(id)).Throws(new InvalidParameterException());
+        var controller = new AdminController
+            (adminService, chartService, requestService, fireBaseService);
+
+        // Act
+        var result = controller.GetById("");
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Should().BeOfType(typeof(ObjectResult));
+        Assert.Equal(StatusCodes.Status400BadRequest,
+            result.GetType().GetProperty("StatusCode")?.GetValue(result));
+    }
+
+    [Fact]
+    public void AdminController_Create_ReturnCreated()
+    {
+        // Arrange
+        CreateAdminModel create = new();
+        A.CallTo(() => adminService.Add(create)).Returns<AdminExtraModel>(new());
+        var controller = new AdminController
+            (adminService, chartService, requestService, fireBaseService);
+
+        // Act
+        var result = controller.Create(create);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Should().BeOfType(typeof(Task<ActionResult>));
+        Assert.Equal(StatusCodes.Status201Created,
+            result.Result.GetType().GetProperty("StatusCode")?.GetValue(result.Result));
+    }
+
+    [Fact]
+    public void AdminController_Create_ReturnBadRequest()
+    {
+        // Arrange
+        CreateAdminModel create = new();
+        A.CallTo(() => adminService.Add(create)).Returns<AdminExtraModel>(null);
+        var controller = new AdminController
+            (adminService, chartService, requestService, fireBaseService);
+        controller.ModelState.AddModelError("SessionName", "Required");
+
+        // Act
+        var result = controller.Create(create);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Should().BeOfType(typeof(Task<ActionResult>));
+        Assert.Equal(typeof(InvalidParameterException).ToString(), 
+            result.Exception?.InnerException?.GetType().ToString());
+    }
+
+    [Fact]
+    public void AdminController_Create_ReturnNotFound()
+    {
+        // Arrange
+        CreateAdminModel create = new();
+        A.CallTo(() => adminService.Add(create)).Returns<AdminExtraModel>(null);
+        var controller = new AdminController
+            (adminService, chartService, requestService, fireBaseService);
+
+        // Act
+        var result = controller.Create(create);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Should().BeOfType(typeof(Task<ActionResult>));
+        Assert.Equal(StatusCodes.Status404NotFound,
+            result.Result.GetType().GetProperty("StatusCode")?.GetValue(result.Result));
     }
 }
