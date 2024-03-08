a994b7344dbb4c9908a8ea8588bce4343645ad37
@thinhphamquoc9999 <thinhphamquoc9999@gmail.com>
2024-03-08 20:44:02 +0700

    Create Test Case For CampaignController

---
+++ Unibean.Test/Controllers/CampaignControllerTest.cs
@@ -0,0 +1,17 @@
+﻿using FakeItEasy;
+using Unibean.Service.Services.Interfaces;
+
+namespace Unibean.Test.Controllers;
+
+public class CampaignControllerTest
+{
+    private readonly ICampaignService campaignService;
+
+    private readonly IJwtService jwtService;
+
+    public CampaignControllerTest()
+    {
+        campaignService = A.Fake<ICampaignService>();
+        jwtService = A.Fake<IJwtService>();
+    }
+}
