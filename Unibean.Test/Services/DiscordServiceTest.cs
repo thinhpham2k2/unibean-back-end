b67dacc702d24f469836de18a8c53ea2144d57fd
@thinhphamquoc9999 <thinhphamquoc9999@gmail.com>
2024-03-16 09:02:14 +0700

    Done Test Case For DiscordService

---
+++ Unibean.Test/Services/DiscordServiceTest.cs
@@ -0,0 +1,33 @@
+﻿using FluentAssertions;
+using Unibean.Service.Services;
+
+namespace Unibean.Test.Services;
+
+public class DiscordServiceTest
+{
+    [Fact]
+    public void DiscordService_ValidateUrl_ReturnFalse()
+    {
+        // Arrange
+        string url = "url";
+
+        // Act
+        var result = DiscordService.ValidateUrl(url);
+
+        // Assert
+        Assert.False(result);
+    }
+
+    [Fact]
+    public void DiscordService_ValidateUrl_ReturnTrue()
+    {
+        // Arrange
+        string url = "https://url.com";
+
+        // Act
+        var result = DiscordService.ValidateUrl(url);
+
+        // Assert
+        Assert.True(result);
+    }
+}
