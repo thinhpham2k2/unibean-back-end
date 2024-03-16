7ed3ef82479fda9506ed9a83584daf1469e25d96
@thinhphamquoc9999 <thinhphamquoc9999@gmail.com>
2024-03-16 09:51:20 +0700

    Done Test Case For JwtService

---
+++ Unibean.Test/Services/JwtServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using Unibean.Service.Models.Authens;
+using Unibean.Service.Services;
+
+namespace Unibean.Test.Services;
+
+public class JwtServiceTest
+{
+    [Fact]
+    public void JwtService_GetJwtRequest()
+    {
+        // Arrange
+        string jwtToken = null;
+        var service = new JwtService();
+
+        // Act
+        var result = service.GetJwtRequest(jwtToken);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Should().BeOfType(typeof(JwtRequestModel));
+        Assert.Equal("User", result.Role);
+    }
+}
