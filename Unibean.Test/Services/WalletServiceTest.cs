01de63f5ddc3b8a0a6445160c67008561d87b924
@thinhphamquoc9999 <thinhphamquoc9999@gmail.com>
2024-03-17 10:40:28 +0700

    Done Test Case For WalletService

---
+++ Unibean.Test/Services/WalletServiceTest.cs
@@ -0,0 +1,39 @@
+﻿using FakeItEasy;
+using FluentAssertions;
+using Unibean.Repository.Entities;
+using Unibean.Repository.Repositories.Interfaces;
+using Unibean.Service.Models.Wallets;
+using Unibean.Service.Services;
+
+namespace Unibean.Test.Services;
+
+public class WalletServiceTest
+{
+    private readonly IWalletRepository walletRepository;
+
+    public WalletServiceTest()
+    {
+        walletRepository = A.Fake<IWalletRepository>();
+    }
+
+    [Fact]
+    public void WalletService_Add()
+    {
+        // Arrange
+        string id = "id";
+        CreateWalletModel creation = A.Fake<CreateWalletModel>();
+        A.CallTo(() => walletRepository.Add(A<Wallet>.Ignored)).Returns(new()
+        {
+            Id = id
+        });
+        var service = new WalletService(walletRepository);
+
+        // Act
+        var result = service.Add(creation);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Should().BeOfType(typeof(WalletModel));
+        Assert.Equal(id, result.Id);
+    }
+}
