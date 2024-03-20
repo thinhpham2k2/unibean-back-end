f602b211524ebb802277030a3530252ccf6224a3
@thinhphamquoc9999 <thinhphamquoc9999@gmail.com>
2024-03-20 10:25:01 +0700

    Done Test Case For VoucherTypeRepository

---
+++ Unibean.Test/Repositories/VoucherTypeRepositoryTest.cs
@@ -0,0 +1,134 @@
+﻿using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using Unibean.Repository.Entities;
+using Unibean.Repository.Paging;
+using Unibean.Repository.Repositories;
+
+namespace Unibean.Test.Repositories;
+
+public class VoucherTypeRepositoryTest
+{
+    private static async Task<UnibeanDBContext> UnibeanDBContext()
+    {
+        var options = new DbContextOptionsBuilder<UnibeanDBContext>()
+            .UseInMemoryDatabase(databaseName: Ulid.NewUlid().ToString())
+            .Options;
+        var databaseContext = new UnibeanDBContext(options);
+        databaseContext.Database.EnsureCreated();
+        if (!await databaseContext.VoucherTypes.AnyAsync())
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                databaseContext.VoucherTypes.Add(
+                new VoucherType()
+                {
+                    Id = i.ToString(),
+                    TypeName = "typeName" + i,
+                    Image = "image" + i,
+                    FileName = "fileName" + i,
+                    DateCreated = DateTime.Now,
+                    DateUpdated = DateTime.Now,
+                    Description = "description" + i,
+                    State = true,
+                    Status = true,
+                });
+                await databaseContext.SaveChangesAsync();
+            }
+        }
+        return databaseContext;
+    }
+
+    [Fact]
+    public async void VoucherTypeRepository_Add()
+    {
+        // Arrange
+        string id = Ulid.NewUlid().ToString();
+        var dbContext = await UnibeanDBContext();
+        var repository = new VoucherTypeRepository(dbContext);
+
+        // Act
+        var result = repository.Add(new()
+        {
+            Id = id
+        });
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Should().BeOfType<VoucherType>();
+        Assert.Equal(id, result.Id);
+    }
+
+    [Fact]
+    public async void VoucherTypeRepository_Delete()
+    {
+        // Arrange
+        string id = "1";
+        var dbContext = await UnibeanDBContext();
+        var repository = new VoucherTypeRepository(dbContext);
+
+        // Act & Assert
+        repository.Delete(id);
+        Assert.False((await dbContext.VoucherTypes.FindAsync(id)).Status.Value);
+    }
+
+    [Fact]
+    public async void VoucherTypeRepository_GetAll()
+    {
+        // Arrange
+        bool? state = null;
+        string propertySort = "Id";
+        bool isAsc = true;
+        string search = "";
+        int page = 1;
+        int limit = 10;
+        var dbContext = await UnibeanDBContext();
+        var repository = new VoucherTypeRepository(dbContext);
+
+        // Act
+        var result = repository.GetAll(state, propertySort, isAsc,
+            search, page, limit);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Should().BeOfType<PagedResultModel<VoucherType>>();
+        Assert.Equal(10, result.RowCount);
+    }
+
+    [Fact]
+    public async void VoucherTypeRepository_GetById()
+    {
+        // Arrange
+        string id = "1";
+        var dbContext = await UnibeanDBContext();
+        var repository = new VoucherTypeRepository(dbContext);
+
+        // Act
+        var result = repository.GetById(id);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Should().BeOfType<VoucherType>();
+        Assert.Equal(id, result.Id);
+    }
+
+    [Fact]
+    public async void VoucherTypeRepository_Update()
+    {
+        // Arrange
+        string id = "1";
+        string description = "description";
+        var dbContext = await UnibeanDBContext();
+        var repository = new VoucherTypeRepository(dbContext);
+
+        // Act
+        var existingAccount = await dbContext.VoucherTypes.FindAsync(id);
+        existingAccount.Description = description;
+        var result = repository.Update(existingAccount);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Should().BeOfType<VoucherType>();
+        Assert.Equal(id, result.Id);
+        Assert.Equal(description, result.Description);
+    }
+}
